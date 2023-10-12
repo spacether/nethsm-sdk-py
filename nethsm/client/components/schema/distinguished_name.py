80b6c34b91d04eb07e92a2e6236a82cc1c902ce9
Justin Black <justin.a.black@gmail.com>
2023-10-12 10:52:26 -0700

    Updates client

--- nethsm/client/components/schema/distinguished_name.py
+++ nethsm/client/components/schema/distinguished_name.py
@@ -106,70 +106,49 @@ class DistinguishedNameDict(schemas.immutabledict[str, str]):
     
     @property
     def commonName(self) -> str:
-        return typing.cast(
-            str,
-            self.__getitem__("commonName")
-        )
+        return self.__getitem__("commonName")
     
     @property
     def countryName(self) -> typing.Union[str, schemas.Unset]:
         val = self.get("countryName", schemas.unset)
         if isinstance(val, schemas.Unset):
             return val
-        return typing.cast(
-            str,
-            val
-        )
+        return val
     
     @property
     def stateOrProvinceName(self) -> typing.Union[str, schemas.Unset]:
         val = self.get("stateOrProvinceName", schemas.unset)
         if isinstance(val, schemas.Unset):
             return val
-        return typing.cast(
-            str,
-            val
-        )
+        return val
     
     @property
     def localityName(self) -> typing.Union[str, schemas.Unset]:
         val = self.get("localityName", schemas.unset)
         if isinstance(val, schemas.Unset):
             return val
-        return typing.cast(
-            str,
-            val
-        )
+        return val
     
     @property
     def organizationName(self) -> typing.Union[str, schemas.Unset]:
         val = self.get("organizationName", schemas.unset)
         if isinstance(val, schemas.Unset):
             return val
-        return typing.cast(
-            str,
-            val
-        )
+        return val
     
     @property
     def organizationalUnitName(self) -> typing.Union[str, schemas.Unset]:
         val = self.get("organizationalUnitName", schemas.unset)
         if isinstance(val, schemas.Unset):
             return val
-        return typing.cast(
-            str,
-            val
-        )
+        return val
     
     @property
     def emailAddress(self) -> typing.Union[str, schemas.Unset]:
         val = self.get("emailAddress", schemas.unset)
         if isinstance(val, schemas.Unset):
             return val
-        return typing.cast(
-            str,
-            val
-        )
+        return val
     
     def get_additional_property_(self, name: str) -> typing.Union[schemas.OUTPUT_BASE_TYPES, schemas.Unset]:
         schemas.raise_if_key_known(name, self.__required_keys__, self.__optional_keys__)
--- nethsm/client/components/schema/network_config.py
+++ nethsm/client/components/schema/network_config.py
@@ -63,24 +63,15 @@ class NetworkConfigDict(schemas.immutabledict[str, str]):
     
     @property
     def gateway(self) -> str:
-        return typing.cast(
-            str,
-            self.__getitem__("gateway")
-        )
+        return self.__getitem__("gateway")
     
     @property
     def ipAddress(self) -> str:
-        return typing.cast(
-            str,
-            self.__getitem__("ipAddress")
-        )
+        return self.__getitem__("ipAddress")
     
     @property
     def netmask(self) -> str:
-        return typing.cast(
-            str,
-            self.__getitem__("netmask")
-        )
+        return self.__getitem__("netmask")
     
     def get_additional_property_(self, name: str) -> typing.Union[schemas.OUTPUT_BASE_TYPES, schemas.Unset]:
         schemas.raise_if_key_known(name, self.__required_keys__, self.__optional_keys__)
